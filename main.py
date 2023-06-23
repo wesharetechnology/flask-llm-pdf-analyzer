7a4116fb9fc6f3443d11e6d512addf60c81c22e7
Alexwwwwww <ywanglu1900@gmail.com>
2023-06-23 10:58:05 +0800

    modified algorithm

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import run_llm
 
 LLM_MODEL_PATH = "E:\Model\chatglm-6b"
 
-app = Flask(__name__)  
+app = Flask(__name__) 
 tokenizer = AutoTokenizer.from_pretrained(LLM_MODEL_PATH, trust_remote_code=True, revision="")
 model = AutoModel.from_pretrained(LLM_MODEL_PATH, trust_remote_code=True, revision="").half().cuda()
 model = model.eval()
@@ -15,16 +15,16 @@ model = model.eval()
 @app.route('/')
 def main():
     return render_template('index.html')
-@app.route('/success', methods = ['POST'])  
+@app.route('/success', methods = ['POST'])
 def success():  
-    if request.method == 'POST':  
+    if request.method == 'POST':
         f = request.files['file']
         f.save(f.filename)
         sentences = read_pdf.extract_sentences(f.filename)
         result = run_llm.feed_to_model(sentences, model, tokenizer)
         # return render the result into string
         result_str = json.dumps(result, indent=4, ensure_ascii=False)
-        print(result_str)
+        # print(result_str)
         return render_template("acknowledgement.html", name = f.filename, result = result_str)
 
 if __name__ == '__main__':
--- run_llm.py
+++ run_llm.py
@@ -1,35 +1,45 @@
 import json
 def feed_to_model(sentences, model, tokenizer):
-    responses = {
-                    "chemical_info" : [
-                    ]
-                }
+    responses = {"chemical_info": []}
+    chemical_name = set()
     # put the pdf text in a string
     i = 0
     for sentence in sentences:
         # feed each sentence to the model
         prompt = """Extract "chemical_name" (keyword,content) from the text above, 
                     also content such as "chemical_property", "chemical_hazard" and output in json format.
-                    Do not output anything if there is no chemical name. Output JSON object only."""
+                    Do not output anything if there is no chemical name.
+                    Skip the citation and reference.
+                    Output JSON object only."""
         i += 1
-        input ='{}\n\n{}'.format(sentence, prompt)
-        
-        response, history =  model.chat(tokenizer, input, history = [])
-        print(response)
+        input = "{}\n\n{}".format(sentence, prompt)
+        response, history = model.chat(tokenizer, input, history=[])
+        # bypass the invalid output
+        if not response.isascii():
+            continue
+        # load the json output
         try:
             data = json.loads(response)
-        except:
+        except json.decoder.JSONDecodeError:
             print("cannot load json")
             continue
-        # print(type(data), data)
-        responses["chemical_info"].append(data)
+        # get rid of the repetitive output
+        try:
+            if data["chemical_name"] not in chemical_name:
+                chemical_name.add(data["chemical_name"])
+                responses["chemical_info"].append(data)
+            else:
+                continue
+        except KeyError:
+            continue
+        if i > 10:
+            break
         # print(type(responses), responses)
     return responses
 
 # def feed_to_model(setences, model, tokenizer):
 #     responses = {
 #         "chemical_info": [
-    
 #         ]
 #     }
 #     sentences = ["""TMI (150.6 mg, 0.5 mmol) was dissolved in ultrapure water (20 mL).""",
@@ -39,12 +49,11 @@ def feed_to_model(sentences, model, tokenizer):
 #     the resulting water layer was extracted three times using ethyl acetate.""",
 #     """A combined organic layer was dried over sodium sulfate and concentrated in vacuo to afford TMI-CN in high purity
 #     as a pale yellow oil without further purification (95.2 mg, 95% yield). """]
-    
-#     # prompt = """Extract "chemical_name" (keyword, content) for chemicals that appears in the text above. 
+#     # prompt = """Extract "chemical_name" (keyword, content) for chemicals that appears in the text above.
 #     #             You should also find the "property", "hazard" of the chemicals from Sigma-Aldrich and output in JSON format.
 #     #             Do not output anything if there is no chemical name."""
 #     # prompt = '从上文中，提取"chemical_name"(keyword,content)，包括:"property", "hazard"等类型的实体, 输出json格式内容'
-#     prompt = """Extract "chemical_name" (keyword,content) from the text above, 
+#     prompt = """Extract "chemical_name" (keyword,content) from the text above,
 #                     also content such as "chemical_property", "chemical_hazard" and output in json format.
 #                     Do not output anything if there is no chemical name. Output JSON object only."""
 #     for sentence in sentences:
@@ -56,4 +65,3 @@ def feed_to_model(sentences, model, tokenizer):
 #         responses['chemical_info'].append(data)
 #         print("response", type(responses), responses)
 #     return responses
-        
